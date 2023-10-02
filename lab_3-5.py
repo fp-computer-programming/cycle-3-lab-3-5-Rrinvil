2da669c63dd3e1c4018b84f9216ee8dddff353ca
Rrinvil <144148135+Rrinvil@users.noreply.github.com>
2023-10-02 14:24:10 -0400

    Add files via upload

---
+++ lab_3-5.py
@@ -0,0 +1,35 @@
+import math
+x = math.pow(22,1)
+print(x)
+import time
+print(time.process_time())
+print(time.perf_counter())
+"the times are the same"
+
+
+
+x = 22 ** 1
+print(x)
+import time
+print(time.process_time())
+print(time.perf_counter())
+"the process times were the same"
+
+
+
+
+
+
+
+
+
+
+
+
+"Record the time using time.process_time before and after each calculation (Hint: you may need to store the result of time.process_time in a variable)"
+
+"Add a statement that prints the elapsed time after each statement is processed. Run the program. What do you notice? Write it as a comment."
+
+"Change each statement that records the time to use time.perf_counter instead of time.process_time."
+
+"Run the program again. What do you notice? Write it as a comment."
